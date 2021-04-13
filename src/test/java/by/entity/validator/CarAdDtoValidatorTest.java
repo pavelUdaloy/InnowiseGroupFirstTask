5644e4cbadf5ad2d35e94bd6c0d7d4170ffefba8
pavelUdaloy <paveludaloi@gmail.com>
2021-04-13 16:02:08 +0300

    fixed simple tests to parametrized tests

--- src/test/java/by/entity/validator/CarAdDtoValidatorTest.java
+++ src/test/java/by/entity/validator/CarAdDtoValidatorTest.java
@@ -1,19 +1,37 @@
 package by.entity.validator;
 
-import by.entity.dto.CarAdDto;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CarAdDtoValidatorTest {
-    @Test
-    void shouldValidForNullValue() {
-        CarAdDtoValidator validator = new CarAdDtoValidator();
 
-        CarAdDto carAdDto = new CarAdDto();
-        carAdDto.setAge(1);
+    private CarAdDtoValidator validator;
 
-        Assertions.assertTrue(validator.isValid(11, null));
-        Assertions.assertFalse(validator.isValid(null, null));
-        Assertions.assertFalse(validator.isValid(-1, null));
+    @BeforeAll
+    void init() {
+        validator = new CarAdDtoValidator();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1121212, 312121212, 1, 3, 111111115, Integer.MAX_VALUE})
+    void simplePositiveNumber(Integer number) {
+        Assertions.assertTrue(validator.isValid(number, null));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void simpleNegativeNumber(Integer number) {
+        Assertions.assertFalse(validator.isValid(number, null));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1121212, -312121212, -1, -3, -111111115, Integer.MIN_VALUE})
+    void simpleNull(Integer number) {
+        Assertions.assertFalse(validator.isValid(number, null));
     }
 }
